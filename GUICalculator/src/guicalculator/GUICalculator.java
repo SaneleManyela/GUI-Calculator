455792adeda02acd6beb543f9b07454b484ec45a
SaneleManyela <63672476+SaneleManyela@users.noreply.github.com>
2021-11-14 02:28:23 +0200

    Add files via upload

---
+++ GUICalculator/src/guicalculator/GUICalculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guicalculator;
+
+/**
+ *
+ * @author Sanele
+ */
+public class GUICalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmMain main = new FrmMain();
+        main.setLocationRelativeTo(null);
+        main.setVisible(true);
+        
+    }
+    
+}
